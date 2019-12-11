125b27a781e0e89db7a1dd8858a92fb7e9657998
tntfantuan <283275604@qq.com>
2019-12-11 12:06:36 +0800

    191211
    191211

--- KOA3/src/controllers/workCollectionModel.js
+++ KOA3/src/controllers/workCollectionModel.js
@@ -25,11 +25,13 @@ var workCollectionItem = async (ctx, next) => {
 }
 
 var essayCollect = async (ctx, next) => {
+    let selectData;
     let tb_condition = `wccUseropenkey,wlcOpenkey`;
     let tb_name = 'yh_wccCollect';
     let tb_whereName = `wccUseropenkey = '${ctx.request.body[0].wccUseropenkey}'`;
     await db.sWhereDb(tb_condition, tb_name, tb_whereName).then(data => {
-        console.log(data);
+        selectData = data;
+        // console.log(data);
         if (data.length == 0) {
             let tb_name = `yh_wccCollect(id,wlcOpenkey,wccUseropenkey,workcollectionHeadimg,workcollectionName,workcollectionContent,workcollectionPraise,workcollectionBrowse,workcollectionCollection,workcollectionUptime,workcollectionUserHeadimg,workcollectionUserNickname,workcollectionUserOpenkey) VALUES(0,?,?,?,?,?,?,?,?,?,?,?,?)`;
             let tb_data = [ctx.request.body[0].wlcOpenkey, ctx.request.body[0].wccUseropenkey, ctx.request.body[0].workcollectionHeadimg, ctx.request.body[0].workcollectionName, ctx.request.body[0].workcollectionContent, ctx.request.body[0].workcollectionPraise, ctx.request.body[0].workcollectionBrowse, ctx.request.body[0].workcollectionCollection, ctx.request.body[0].workcollectionUptime, ctx.request.body[0].workcollectionUserHeadimg, ctx.request.body[0].workcollectionUserNickname, ctx.request.body[0].workcollectionUserOpenkey];
@@ -53,6 +55,15 @@ var essayCollect = async (ctx, next) => {
             let tb_name = 'yh_wccCollect';
             let tb_whereName = `wccUseropenkey = '${ctx.request.body[0].wccUseropenkey}' AND wlcOpenkey = '${ctx.request.body[0].wlcOpenkey}'`;
             db.deleteDataDb(tb_name, tb_whereName).then(data => {
+                let tb_name = 'yh_workCollection';
+                let tb_setName = 'workcollectionCollection';
+                let tb_whereName = 'wlcOpenkey';
+                let THISwcCollection = parseInt(ctx.request.body[0].workcollectionCollection) - 2
+                let tb_data = { tb_setData: THISwcCollection, tb_whereData: ctx.request.body[0].wlcOpenkey };
+                db.upDataDb(tb_name, tb_setName, tb_whereName, tb_data).then(data => {
+                }).catch(err => {
+                    console.log(err);
+                });
             }).catch(err => {
                 console.log(err)
                 ctx.response.body = '取消收藏失败';
@@ -65,8 +76,56 @@ var essayCollect = async (ctx, next) => {
 }
 
 var essayPraise = async (ctx, next) => {
-    console.log(ctx.request.body);
+    // console.log(ctx.request.body);
+    let selectData;
+    let tb_condition = `wccUseropenkey,wlcOpenkey`;
+    let tb_name = 'yh_wccPraise';
+    let tb_whereName = `wccUseropenkey = '${ctx.request.body[0].wccUseropenkey}'`;
+    await db.sWhereDb(tb_condition, tb_name, tb_whereName).then(data => {
+        console.log(data);
+        selectData = data;
+        if (data.length == 0) {
+            let tb_name = `yh_wccPraise(id,wlcOpenkey,wccUseropenkey,workcollectionHeadimg,workcollectionName,workcollectionContent,workcollectionPraise,workcollectionBrowse,workcollectionCollection,workcollectionUptime,workcollectionUserHeadimg,workcollectionUserNickname,workcollectionUserOpenkey) VALUES(0,?,?,?,?,?,?,?,?,?,?,?,?)`;
+            let tb_data = [ctx.request.body[0].wlcOpenkey, ctx.request.body[0].wccUseropenkey, ctx.request.body[0].workcollectionHeadimg, ctx.request.body[0].workcollectionName, ctx.request.body[0].workcollectionContent, ctx.request.body[0].workcollectionPraise, ctx.request.body[0].workcollectionBrowse, ctx.request.body[0].workcollectionCollection, ctx.request.body[0].workcollectionUptime, ctx.request.body[0].workcollectionUserHeadimg, ctx.request.body[0].workcollectionUserNickname, ctx.request.body[0].workcollectionUserOpenkey];
+            db.insertDatabase(tb_name, tb_data).then(data => {
 
+                let tb_name = 'yh_workCollection';
+                let tb_setName = 'workcollectionPraise';
+                let tb_whereName = 'wlcOpenkey';
+                let tb_data = { tb_setData: ctx.request.body[0].workcollectionPraise, tb_whereData: ctx.request.body[0].wlcOpenkey };
+
+                db.upDataDb(tb_name, tb_setName, tb_whereName, tb_data).then(data => { }).catch(err => {
+                    console.log(err);
+                });
+            }).catch(err => {
+                console.log(err);
+            });
+            ctx.response.body = '200';
+
+        } else if (data.length == 1) {
+            let tb_name = 'yh_wccPraise';
+            let tb_whereName = `wccUseropenkey = '${ctx.request.body[0].wccUseropenkey}' AND wlcOpenkey = '${ctx.request.body[0].wlcOpenkey}'`;
+            db.deleteDataDb(tb_name, tb_whereName).then(data => {
+
+                let tb_name = 'yh_workCollection';
+                let tb_setName = 'workcollectionPraise';
+                let tb_whereName = 'wlcOpenkey';
+                let THISwcPraise = parseInt(ctx.request.body[0].workcollectionPraise) - 2
+                let tb_data = { tb_setData: THISwcPraise, tb_whereData: ctx.request.body[0].wlcOpenkey };
+
+                db.upDataDb(tb_name, tb_setName, tb_whereName, tb_data).then(data => {
+                }).catch(err => {
+                    console.log(err);
+                });
+            }).catch(err => {
+                console.log(err)
+                ctx.response.body = '取消点赞失败';
+            });
+            ctx.response.body = '400';
+        }
+    }).catch(err => {
+        console.log(err);
+    });
 }
 
 
